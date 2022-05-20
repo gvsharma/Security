50f89eea52e42e9a941a47f150645039d8b8710c
Gv Sharma <gvsharma4@gmail.com>
2022-05-20 23:32:18 +0530

    added CommentEntity.java

--- src/main/java/com/security/security/entities/CommentEntity.java
+++ src/main/java/com/security/security/entities/CommentEntity.java
@@ -1,4 +1,28 @@
 package com.security.security.entities;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity(name = "comments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CommentEntity extends BaseEntity {
+    private String body;
+    private UserEntity author;
+    private ArticleEntity article;
+
+    @ManyToOne(targetEntity = UserEntity.class)
+    public UserEntity getAuthor() {
+        return author;
+    }
+
+    @ManyToOne(targetEntity = ArticleEntity.class)
+    public ArticleEntity getArticle() {
+        return article;
+    }
 }
