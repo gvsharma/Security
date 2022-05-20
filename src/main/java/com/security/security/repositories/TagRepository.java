521355f127315586249d20719424eec4819ddb76
Gv Sharma <gvsharma4@gmail.com>
2022-05-21 00:32:38 +0530

    added TagRepository.java

---
+++ src/main/java/com/security/security/repositories/TagRepository.java
@@ -0,0 +1,12 @@
+package com.security.security.repositories;
+
+import com.security.security.entities.TagEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TagRepository extends JpaRepository<TagEntity, Long> {
+    Optional<TagEntity> findByName(String name);
+}
